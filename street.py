0ce674ff8bfb3ff882a590d717187fb48f57f8bb
Xarrow-008 <leox.pages@gmail.com>
2025-03-12 14:38:27 +0100

    pouuuushshshshshsh

--- street.py
+++ street.py
@@ -5,6 +5,7 @@ from random import*
 
 up()
 bk(700)
+speed(10)
 nbbats=5
 def etage(color):
     down()
@@ -21,15 +22,41 @@ def etage(color):
     end_fill()
     up()
 
-def fenetre():
-    fillcolor("white")
-    begin_fill()
-    down()
-    for i in range(4):
-        fd(30)
-        lt(90)
-    end_fill()
-    up()
+def fenetre(proba):
+    if proba!=0:
+        fillcolor("white")
+        begin_fill()
+        down()
+        for i in range(4):
+            fd(30)
+            lt(90)
+        end_fill()
+        up()
+        if proba>=6:
+            fillcolor("brown")
+            lt(90)
+            fd(10)
+            lt(90)
+            fd(5)
+            lt(90)
+            begin_fill()
+            down()
+            fd(15)
+            lt(90)
+            fd(40)
+            lt(90)
+            fd(15)
+            lt(90)
+            fd(40)
+            lt(90)
+            end_fill()
+            up()
+            lt(90)
+            fd(5)
+            rt(90)
+            fd(10)
+            lt(90)
+
 
 def porte():
     fillcolor("blue")
@@ -46,24 +73,32 @@ def porte():
     end_fill()
     up()
 
-def deco_etage(porte):
+def deco_etage(etage):
     a=randint(0,2)
     fd(15)
     up()
     for i in range(3):
-        if i==a and porte=='porte':
+        if i==a and etage==0:
             porte()
         else:
+            if etage != 0:
+                b=randint(0,9)
+            else:
+                b=randint(0,5)
             lt(90)
             fd(10)
             rt(90)
-            fenetre()
+            fenetre(b)
             rt(90)
             fd(10)
             lt(90)
         fd(40)
     bk(135)
+
+def toit(proba):
+    fd(150)
     
+
 
 
 
@@ -73,11 +108,13 @@ for i in range(nbbats):
     nbEtages=randint(1,5)
     for i in range(nbEtages):
         etage("red")
-        deco_etage(porte)
+        deco_etage(i)
         lt(90)
         fd(50)
         rt(90)
-    rt(90)
+    c=randint(0,2)
+    toit(c)
+    rt(90)#descendre tt les etages
     fd(nbEtages*50)
     lt(90)
     fd(150)
