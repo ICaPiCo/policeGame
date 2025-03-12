3316e840e19b69063b774e47ec36e55a481199fe
Xarrow-008 <leox.pages@gmail.com>
2025-03-12 22:23:59 +0100

    poush da roofs

--- street.py
+++ street.py
@@ -1,5 +1,6 @@
 from turtle import*
 from random import*
+from math import*
 #Fillcolor()          begin.fill()            end.fill()
 #10 15 30 10 30 10 30 15 10
 
@@ -7,50 +8,31 @@ up()
 bk(700)
 speed(10)
 nbbats=5
-def etage(color):
-    down()
+def rectangle(lenght,width,color):
     fillcolor(color)
     begin_fill()
-    fd(140)
-    lt(90)
-    fd(50)
-    lt(90)
-    fd(140)
-    lt(90)
-    fd(50)
-    lt(90)
+    down()
+    for i in range(2):
+        fd(lenght)
+        lt(90)
+        fd(width)
+        lt(90)
     end_fill()
     up()
+def etage(color):
+    down()
+    rectangle(140,50,'red')
 
 def fenetre(proba):
     if proba!=0:
-        fillcolor("white")
-        begin_fill()
-        down()
-        for i in range(4):
-            fd(30)
-            lt(90)
-        end_fill()
-        up()
+        rectangle(30,30,'white')
         if proba>=6:
-            fillcolor("brown")
             lt(90)
             fd(10)
             lt(90)
             fd(5)
             lt(90)
-            begin_fill()
-            down()
-            fd(15)
-            lt(90)
-            fd(40)
-            lt(90)
-            fd(15)
-            lt(90)
-            fd(40)
-            lt(90)
-            end_fill()
-            up()
+            rectangle(15,40,'brown')
             lt(90)
             fd(5)
             rt(90)
@@ -59,19 +41,7 @@ def fenetre(proba):
 
 
 def porte():
-    fillcolor("blue")
-    begin_fill()
-    down()
-    fd(30)
-    lt(90)
-    fd(45)
-    lt(90)
-    fd(30)
-    lt(90)
-    fd(45)
-    lt(90)
-    end_fill()
-    up()
+    rectangle(30,45,'blue')
 
 def deco_etage(etage):
     a=randint(0,2)
@@ -96,15 +66,45 @@ def deco_etage(etage):
     bk(135)
 
 def toit(proba):
-    fd(150)
+    if proba==0:
+        fillcolor('black')
+        down()
+        begin_fill()
+        fd(150)
+        lt(110)
+        fd(80/(cos(70*pi/180)))
+        lt(140)
+        fd(80/(cos(70*pi/180)))
+        lt(110)
+        fd(10)
+        up()
+        end_fill()
+    elif proba==1:
+        bk(5)
+        rectangle(150,10,'black')
+    else:
+        fillcolor('black')
+        down()
+        begin_fill()
+        fd(150)
+        lt(150)
+        fd(80/(cos(30*pi/180)))
+        lt(60)
+        fd(80/(cos(30*pi/180)))
+        lt(150)
+        fd(10)
+        up()
+        end_fill()
+        
     
 
 
 
 
 
+
 for i in range(nbbats):
-    fd(10)
+    fd(25)
     nbEtages=randint(1,5)
     for i in range(nbEtages):
         etage("red")
@@ -113,7 +113,9 @@ for i in range(nbbats):
         fd(50)
         rt(90)
     c=randint(0,2)
+    c=2
     toit(c)
+
     rt(90)#descendre tt les etages
     fd(nbEtages*50)
     lt(90)
