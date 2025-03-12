2720319f4d96099d734f980cfb12e9a2acf979c0
Xarrow-008 <leox.pages@gmail.com>
2025-03-12 13:55:37 +0100

    poushh

--- street.py
+++ street.py
@@ -21,23 +21,65 @@ def etage(color):
     end_fill()
     up()
 
+def fenetre():
+    fillcolor("white")
+    begin_fill()
+    down()
+    for i in range(4):
+        fd(30)
+        lt(90)
+    end_fill()
+    up()
+
 def porte():
-    a=randint(0,3)
+    fillcolor("blue")
+    begin_fill()
+    down()
+    fd(30)
+    lt(90)
+    fd(45)
+    lt(90)
+    fd(30)
+    lt(90)
+    fd(45)
+    lt(90)
+    end_fill()
+    up()
+
+def deco_etage(porte):
+    a=randint(0,2)
     fd(15)
+    up()
     for i in range(3):
-        if i==a:
-            beginfill("blue")
-            fd()
+        if i==a and porte=='porte':
+            porte()
+        else:
+            lt(90)
+            fd(10)
+            rt(90)
+            fenetre()
+            rt(90)
+            fd(10)
+            lt(90)
+        fd(40)
+    bk(135)
+    
 
 
 
 
 for i in range(nbbats):
     fd(10)
-
-    etage("red")
-    lt(90)
-    fd(50)
+    nbEtages=randint(1,5)
+    for i in range(nbEtages):
+        etage("red")
+        deco_etage(porte)
+        lt(90)
+        fd(50)
+        rt(90)
     rt(90)
+    fd(nbEtages*50)
+    lt(90)
+    fd(150)
 
 done()
