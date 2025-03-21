ab4f58b36d153c634406c12531e25f09d38ac13c
sam1onearth <samuel@greenspinach.com>
2025-03-21 19:14:19 +0100

    I suggest we ORGANISE
    CLASSES ARE SO USEFUL LETS GOO
    heres a brief intro, me messing around with classes, feel free to play around

---
+++ Intro to classes.py
@@ -0,0 +1,37 @@
+import pygame as pyg
+
+class Item:
+    counted = 0
+    def __init__(self,name,damage,weight):
+        self.name = name
+        self.damage = damage
+        self.weight = weight
+        Item.counted +=1
+    @classmethod
+    def from_dict(cls,data):
+        return cls(data["name"], data["damage"], data["weight"])
+    
+    @staticmethod
+    def is_heavy(weight):
+        return weight>10
+    @classmethod
+    def count(cls):
+        return cls.counted
+class Weapon(Item):
+    weapon_count = 0
+    def __init__(self,name, damage, weight, durability):
+        super().__init__(name, damage, weight)
+        self.durability = durability
+        Weapon.weapon_count +=1
+    @classmethod
+    def weaponCount(cls):
+        return cls.weapon_count
+    
+        
+        
+sword = Weapon("sword",20,30,10)
+print(sword.is_heavy(sword.weight))
+print(Weapon.weaponCount())
+stick = Item("stick",10,40)
+print(stick.is_heavy(stick.weight))
+print(Item.count())
